423f04677bdb3ba591a32cebe8a2bc62ab487738
Rigan Jevi <righan.j13@gmail.com>
2026-08-06 00:24:08 +0700

    Update frontend, Vite build assets, and Bidan pages

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,23 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js',  'resources/css/inspired-campus.css','resources/js/app.js', 'resources/css/prodi.css', 'resources/css/fisiotrapi.css', 'resources/css/ners.css', 'resources/css/dkv.css',
-            'resources/css/bidan.css', 'resources/css/bisdig.css', 'resources/css/farmasi.css', 'resources/css/kwu.css', 'resources/css/pariwisata.css', 'resources/css/psikologi.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/inspired-campus.css',
+                'resources/css/prodi.css',
+                'resources/css/fisiotrapi.css',
+                'resources/css/ners.css',
+                'resources/css/dkv.css',
+                'resources/css/bidan.css',
+                'resources/css/bisdig.css',
+                'resources/css/farmasi.css',
+                'resources/css/kwu.css',
+                'resources/css/pariwisata.css',
+                'resources/css/psikologi.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
     ],
-    });
+});
